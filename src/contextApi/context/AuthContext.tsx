b158e2bd171e2dd7140b409e69289e740ea2fca9
Cleuber Andrade <cleuber_binho@hotmail.com>
2022-02-09 13:45:09 -0300

    Update AuthContext.tsx

--- src/contextApi/context/AuthContext.tsx
+++ src/contextApi/context/AuthContext.tsx
@@ -87,7 +87,7 @@ export function AuthProvider(props) {
         try {
             setCarregando(true)
             const resp = await firebase.auth().signInWithPopup(
-                new firebase.auth.GoogleAuthProvider()
+              new firebase.auth.GoogleAuthProvider()
             )
 
             await configurarSessao(resp.user)
