bdf4584a04986ca7ff666e2c94e41066ecce545d
Cleuber Andrade <cleuber_binho@hotmail.com>
2022-02-17 19:17:46 -0300

    Update Tabela.tsx

--- src/components/templates/Tabela.tsx
+++ src/components/templates/Tabela.tsx
@@ -26,16 +26,16 @@ const Tabela = ({clientes}: TabelaProps) => {
     return clientes?.map((cliente, i) => {
       return (
         <tr key={cliente.id} className={` ${i % 2 === 0 ? "bg-white bg-opacity-40" : "bg-white bg-opacity-20"}`}>
-          <td className=" p-4">{cliente.id}</td>
-          <td className=" p-4">{cliente.nome}</td>
-          <td className=" p-4">{cliente.cpf}</td>
-          <td className=" p-4">{cliente.carteira}</td>
-          <td className=" p-4">{cliente.endereco}</td>
-          <td className=" p-4">{cliente.nascimento}</td>
-          <td className=" p-4">{cliente.tel}</td>
-          <td className=" p-4">{cliente.plano}</td>
-          <td className=" p-4">{cliente.especialidade}</td>
-          <td className=" p-4">{cliente.doutor}</td>
+          <td className="text-center p-4">{cliente.id}</td>
+          <td className="text-center p-4">{cliente.nome}</td>
+          <td className="text-center p-4">{cliente.cpf}</td>
+          <td className="text-center p-4">{cliente.carteira}</td>
+          <td className="text-center p-4">{cliente.endereco}</td>
+          <td className="text-center p-4">{cliente.nascimento}</td>
+          <td className="text-center p-4">{cliente.tel}</td>
+          <td className="text-center p-4">{cliente.plano}</td>
+          <td className="text-center p-4">{cliente.especialidade}</td>
+          <td className="text-center p-4">{cliente.doutor}</td>
         </tr>
       )
     })
