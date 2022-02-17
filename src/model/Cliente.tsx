719d47ed7868c43da58e54cc528829a72de23671
Cleuber Andrade <cleuber_binho@hotmail.com>
2022-02-17 16:06:12 -0300

    Update Cliente.tsx

--- src/model/Cliente.tsx
+++ src/model/Cliente.tsx
@@ -10,8 +10,22 @@ export default class Cliente {
   #especialidade: string
   #doutor: string
 
-  constructor(id: string = null, nome: string, cpf: number, carteira: string, endereco: string, nascimento: number, tel: string,  plano: string, especialidade: string, doutor: string){
+  constructor(nome: string, cpf: number, carteira: string, endereco: string, nascimento: number, tel: string,  plano: string, especialidade: string, doutor: string, id: string = null){
     this.#nome = nome
-
+    this.#cpf = cpf
+    this.#carteira = carteira
+    this.#endereco = endereco
+    this.#nascimento = nascimento
+    this.#tel = tel
+    this.#plano = plano
+    this.#especialidade = especialidade
+    this.#doutor = doutor
+    this.#id = id
   }
+
+  get id(){
+    return this.#id
+  }
+
+  
 }
