f5847a537fe5a1e18b8e5b8f97bbd2fa7b61d9e8
Cleuber Andrade <cleuber_binho@hotmail.com>
2022-02-17 19:29:09 -0300

    Update Tabela.tsx

--- src/components/templates/Tabela.tsx
+++ src/components/templates/Tabela.tsx
@@ -7,17 +7,17 @@ const Tabela = ({clientes}: TabelaProps) => {
 
   const renderizarCabecalho = () =>{
     return (
-      <tr>
-        <th className="pb-5">Id</th>
-        <th className="pb-5">Nome</th>
-        <th className="pb-5">CPF</th>
-        <th className="pb-5">Carteira</th>
-        <th className="pb-5">Endereco</th>
-        <th className="pb-5">Nascimento</th>
-        <th className="pb-5">Telefone</th>
-        <th className="pb-5">Plano</th>
-        <th className="pb-5">Especialidade</th>
-        <th className="pb-5">Doutor</th>
+      <tr className="bg-gray-500">
+        <th className="p-3">Id</th>
+        <th className="p-3">Nome</th>
+        <th className="p-3">CPF</th>
+        <th className="p-3">Carteira</th>
+        <th className="p-3">Endereco</th>
+        <th className="p-3">Nascimento</th>
+        <th className="p-3">Telefone</th>
+        <th className="p-3">Plano</th>
+        <th className="p-3">Especialidade</th>
+        <th className="p-3">Doutor</th>
       </tr>
     )
   }
@@ -26,23 +26,23 @@ const Tabela = ({clientes}: TabelaProps) => {
     return clientes?.map((cliente, i) => {
       return (
         <tr key={cliente.id} className={` ${i % 2 === 0 ? "bg-white bg-opacity-40" : "bg-white bg-opacity-20"}`}>
-          <td className="text-center p-4">{cliente.id}</td>
-          <td className="text-center p-4">{cliente.nome}</td>
-          <td className="text-center p-4">{cliente.cpf}</td>
-          <td className="text-center p-4">{cliente.carteira}</td>
-          <td className="text-center p-4">{cliente.endereco}</td>
-          <td className="text-center p-4">{cliente.nascimento}</td>
-          <td className="text-center p-4">{cliente.tel}</td>
-          <td className="text-center p-4">{cliente.plano}</td>
-          <td className="text-center p-4">{cliente.especialidade}</td>
-          <td className="text-center p-4">{cliente.doutor}</td>
+          <td className="text-center p-3">{cliente.id}</td>
+          <td className="text-center p-3">{cliente.nome}</td>
+          <td className="text-center p-3">{cliente.cpf}</td>
+          <td className="text-center p-3">{cliente.carteira}</td>
+          <td className="text-center p-3">{cliente.endereco}</td>
+          <td className="text-center p-3">{cliente.nascimento}</td>
+          <td className="text-center p-3">{cliente.tel}</td>
+          <td className="text-center p-3">{cliente.plano}</td>
+          <td className="text-center p-3">{cliente.especialidade}</td>
+          <td className="text-center p-3">{cliente.doutor}</td>
         </tr>
       )
     })
   }
 
   return(
-    <table className="w-full">
+    <table className="w-full rounded-md overflow-hidden">
       <thead className="text-xl">
         {renderizarCabecalho()}
       </thead>
